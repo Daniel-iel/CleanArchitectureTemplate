ebfed766b89bed1ac02cb7cbc72095d3338d44d2
Daniel-iel <iel_182@hotmail.com>
2025-07-06 13:26:54 -0300

    Ajustando DI da Api

---
+++ src/RideSharingApp.Api/DependencyInjection.cs
@@ -0,0 +1,99 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using RideSharingApp.Api.Endpoints;
+using System.Text;
+
+namespace RideSharingApp.Api;
+
+public static class DependencyInjection
+{
+    public static WebApplication UseApi(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapUserEndpoints();
+        app.MapSubscriptionsEndpoints();
+        app.MapRideEndpoints();
+
+        return app;
+    }
+
+    public static IServiceCollection AddApi(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddVersion(configuration)
+            .AddAuth()
+            .AddSwaggerGen();
+
+        return services;
+    }
+
+    private static IServiceCollection AddVersion(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+          .AddJwtBearer(options =>
+          {
+              options.TokenValidationParameters = new TokenValidationParameters
+              {
+                  ValidateIssuer = false,
+                  ValidateAudience = false,
+                  ValidateLifetime = true,
+                  ValidateIssuerSigningKey = true,
+                  IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"] ?? "supersecretkey"))
+              };
+          });
+
+        services.AddAuthorization();
+
+        return services;
+    }
+
+    private static IServiceCollection AddAuth(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ReportApiVersions = true;
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                new UrlSegmentApiVersionReader(),
+                new HeaderApiVersionReader("x-api-version"),
+                new MediaTypeApiVersionReader("x-api-version")
+            );
+        })
+       .AddMvc()
+       .AddApiExplorer(options =>
+       {
+           options.GroupNameFormat = "'v'VVV";
+           options.SubstituteApiVersionInUrl = true;
+       });
+
+        services.AddEndpointsApiExplorer();
+
+        return services;
+    }
+
+    private static IServiceCollection AddSwaggerGen(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+            {
+                Title = "Ride Sharing API",
+                Version = "v1",
+                Description = "API for Ride Sharing Application"
+            });
+        });
+        return services;
+    }
+
+}
