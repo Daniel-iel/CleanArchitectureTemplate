35570a03a6c3e911258b5992d3faaac56115c829
Daniel-iel <iel_182@hotmail.com>
2025-07-06 13:27:48 -0300

    Ajustando Di do SharedKernel

---
+++ src/RideSharingApp.SharedKernel/DependecyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RideSharingApp.SharedKernel
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddSharedKernel(this IServiceCollection services)
+        {
+            // Register shared kernel services here, if any
+            // For example, logging, caching, etc.
+
+            return services;
+        }
+    }
+}
