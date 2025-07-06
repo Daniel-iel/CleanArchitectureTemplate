a93f445ef7f856887a507239bbfc05571d7a6b78
Daniel-iel <iel_182@hotmail.com>
2025-07-06 13:27:27 -0300

    Ajustando DI da infra

--- src/RideSharingApp.Infrastructure/DependecyInjection.cs
+++ src/RideSharingApp.Infrastructure/DependecyInjection.cs
@@ -1,3 +1,47 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RideSharingApp.Application.Common.Interfaces;
+using RideSharingApp.Infrastructure.Database;
+using RideSharingApp.Infrastructure.Database.Login;
+using RideSharingApp.Infrastructure.Database.Rides;
+using RideSharingApp.Infrastructure.Database.Subscriptions;
+
 namespace RideSharingApp.Infrastructure;
 
-public class DependecyInjection{}
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddMigration(configuration)
+            .AddRepositories();
+
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRideRepository, RideRepository>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddMigration(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Executa a migration Flyway ao subir a aplicação
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        if (!string.IsNullOrWhiteSpace(connectionString))
+        {
+            MigrateDatabase.RunFlywayMigration(connectionString);
+        }
+        else
+        {
+            Console.WriteLine("A string de conexão para o banco de dados não foi encontrada. Migration não executada.");
+        }
+
+        return services;
+    }
+}
